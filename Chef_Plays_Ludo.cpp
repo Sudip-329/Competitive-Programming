98376f00a41b148a5d0b4d28079bf9ab45bc2272
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-01-11 23:31:20 +0600

    Starter 73 : Problem A

---
+++ Chef_Plays_Ludo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x==6)
+	    {
+	        cout<<"YES";
+	    }
+	    else
+	    {
+	        cout<<"NO";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
