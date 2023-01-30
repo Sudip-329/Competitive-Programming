f485191440dbbb78a306114ac3ab847c4817cd34
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-01-30 10:55:39 +0530

    Codeshef

---
+++ Overspeeding_Fine.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>70 and x<=100)
+	    {
+	        cout<<500<<endl;
+	    }
+	    else if(x>100)
+	    {
+	        cout<<2000<<endl;
+	    }
+	    else 
+	    {
+	        cout<<0<<endl;
+	    }
+	}
+	return 0;
+}
