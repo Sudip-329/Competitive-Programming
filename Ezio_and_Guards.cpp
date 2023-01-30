d49a854ecf4e3f9f362922e41c97ff0dd82e7d4c
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-01-30 12:12:43 +0530

    Codeshef

---
+++ Ezio_and_Guards.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	    cout<<"YES";
+	    }
+	    else
+	    {
+	        cout<<"NO";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
