aaea818174aefb2d4620eef27c9c642ab1dfe64b
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-01-31 10:21:24 +0530

    Codeforce

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);     
+    cin.tie(0);
+    long long t;
+    cin>>t;
+    cout<<25;
+
+}
+      
