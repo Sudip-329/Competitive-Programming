c26cf180b4aa13a05cff27221d40b0593e4159ef
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-23 14:06:14 +0530

    Codeforce

---
+++ A_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int h,m;
+        cin>>h>>m;
+        cout<<(1440-((h*60)+m))<<endl;
+    }
+}
