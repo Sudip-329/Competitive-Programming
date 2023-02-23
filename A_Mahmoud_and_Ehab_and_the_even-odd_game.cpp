12cd048e556f3da893fa4f06f85c29790277f6dd
Sudip-329 <108211139+Sudip-329@users.noreply.github.com>
2023-02-23 14:12:36 +0530

    Codeforce

---
+++ A_Mahmoud_and_Ehab_and_the_even-odd_game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	
+	int t;
+	cin>>t;
+    if(t&1)
+    {
+        cout<<"Ehab"<<endl;
+    }
+    else{
+        cout<<"Mahmoud"<<endl;
+    }
+
+}
