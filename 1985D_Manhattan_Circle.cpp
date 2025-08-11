a852521814128c19c8f341f6115669243ca0cb62
Sudip-329 <sudipchakrabarty6@gmail.com>
2025-08-11 13:19:05 +0600

    Create 1985D_Manhattan_Circle.cpp

---
+++ 1985D_Manhattan_Circle.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<string> grid(n);
+        for (int i = 0; i < n; i++) {
+            cin >> grid[i];
+        }
+
+        int top = n, bottom = -1, left = m, right = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '#') {
+                    top = min(top, i);
+                    bottom = max(bottom, i);
+                    left = min(left, j);
+                    right = max(right, j);
+                }
+            }
+        }
+
+        int centerRow = (top + bottom) / 2 + 1;
+        int centerCol = (left + right) / 2 + 1;
+
+        cout << centerRow << " " << centerCol << "\n";
+    }
+
+    return 0;
+}
