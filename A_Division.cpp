da7340bfd1158e831584eb208cb36d316ecd4669
Sudip-329 <sudipchakrabarty6@gmail.com>
2025-08-29 01:38:45 +0600

    Add files via upload

---
+++ A_Division.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int rating;
+        cin>>rating;
+        if(rating>=1900)
+        {
+            cout<<"Division 1"<<endl;
+        }
+        else if(rating>=1600 && rating <=1899)
+        {
+            cout<<"Division 2"<<endl;
+        }
+        else if(rating>=1400 && rating <=1599)
+        {
+            cout<<"Division 3"<<endl;
+        }
+        else if (rating<=1399)
+        {
+            cout<<"Division 4"<<endl;
+        }
+    }
+}
