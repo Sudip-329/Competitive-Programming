5cea3d3b11ae94b6694d7fd52614954250fd1d71
Sudip-329 <sudipchakrabarty6@gmail.com>
2026-02-21 00:54:55 +0600

    Add files via upload

--- B_Make_it_Zigzag.cpp
+++ B_Make_it_Zigzag.cpp
@@ -1,95 +1,60 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define int long long
-// #define INF (int)1e18
-
-// void processTestCase() {
-//     int length;
-//     cin >> length;
-    
-//     vector<int> original(length), adjusted(length);
-//     int maxSoFar = 0;
-    
-//     for (int idx = 0; idx < length; ++idx) {
-//         cin >> original[idx];
-//         adjusted[idx] = original[idx];
-//         maxSoFar = max(maxSoFar, original[idx]);
-//         if (idx % 2 != 0) {
-//             adjusted[idx] = maxSoFar;
-//         }
-//     }
-    
-//     int totalDecrement = 0;
-    
-//     for (int idx = 0; idx < length; idx += 2) {
-//         long long upperBound = -1;
-        
-//         if (idx > 0 && idx < length - 1)
-//             upperBound = min(adjusted[idx - 1], adjusted[idx + 1]);
-//         else if (idx == 0 && length > 1)
-//             upperBound = adjusted[idx + 1];
-//         else if (idx == length - 1 && length > 1)
-//             upperBound = adjusted[idx - 1];
-        
-//         if (upperBound != -1 && original[idx] >= upperBound) {
-//             totalDecrement += original[idx] - (upperBound - 1);
-//         }
-//     }
-    
-//     cout << totalDecrement << "\n";
-// }
-
-// int32_t main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-    
-//     int testCases;
-//     cin >> testCases;
-//     while (testCases--) {
-//         processTestCase();
-//     }
-    
-//     return 0;
-// }
-
-
-
-
-
-
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+#define INF (int)1e18
 
-void solveZigzag() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i) cin >> arr[i];
-
-    int totalCost = 0;
-    for (int i = 0; i < n; i += 2) {
-        long long left = (i > 0) ? arr[i - 1] : LLONG_MAX;
-        long long right = (i + 1 < n) ? arr[i + 1] : LLONG_MAX;
-
-        long long maxNeighbor = min(left, right);
-        if (arr[i] >= maxNeighbor) {
-            totalCost += arr[i] - (maxNeighbor - 1);
+void processTestCase() {
+    int length;
+    cin >> length;
+    
+    vector<int> original(length), adjusted(length);
+    int maxSoFar = 0;
+    
+    for (int idx = 0; idx < length; ++idx) {
+        cin >> original[idx];
+        adjusted[idx] = original[idx];
+        maxSoFar = max(maxSoFar, original[idx]);
+        if (idx % 2 != 0) {
+            adjusted[idx] = maxSoFar;
         }
     }
-
-    cout << totalCost << "\n";
+    
+    int totalDecrement = 0;
+    
+    for (int idx = 0; idx < length; idx += 2) {
+        long long upperBound = -1;
+        
+        if (idx > 0 && idx < length - 1)
+            upperBound = min(adjusted[idx - 1], adjusted[idx + 1]);
+        else if (idx == 0 && length > 1)
+            upperBound = adjusted[idx + 1];
+        else if (idx == length - 1 && length > 1)
+            upperBound = adjusted[idx - 1];
+        
+        if (upperBound != -1 && original[idx] >= upperBound) {
+            totalDecrement += original[idx] - (upperBound - 1);
+        }
+    }
+    
+    cout << totalDecrement << "\n";
 }
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int t;
-    cin >> t;
-    while (t--) solveZigzag();
+    int testCases;
+    cin >> testCases;
+    while (testCases--) {
+        processTestCase();
+    }
+    
     return 0;
 }
+
+
+
+
+
+
+
